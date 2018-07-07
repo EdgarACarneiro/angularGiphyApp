5d0d2991b92e9679c4eef4f6a4db37254934a1ff
EdgarACarneiro <up201503784@fe.up.pt>
2018-07-07 20:29:11 +0100

    Search functionality, missin API call

--- angularGiphyApp/src/app/app.module.ts
+++ angularGiphyApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FunctionalitiesComponent } from './functionalities/functionalities.component';
@@ -11,10 +12,11 @@ import { GiphsContainerComponent } from './giphs-container/giphs-container.compo
     AppComponent,
     FunctionalitiesComponent,
     SearchComponent,
-    GiphsContainerComponent
+    GiphsContainerComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angularGiphyApp/src/app/search/search.component.ts
+++ angularGiphyApp/src/app/search/search.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  query : string;
+
   constructor() { }
 
   ngOnInit() {
+    this.query = '';
+  }
+
+  onSubmit() {
+    // TODO: use value of query to ask for gifs
   }
 
 }
