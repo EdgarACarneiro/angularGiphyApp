c2327381bdead9be83979e61263ab61fe202665c
EdgarACarneiro <up201503784@fe.up.pt>
2018-07-07 20:55:09 +0100

    Base of gif container

--- angularGiphyApp/src/app/giphs-container/giphs-container.component.ts
+++ angularGiphyApp/src/app/giphs-container/giphs-container.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GiphsContainerComponent implements OnInit {
 
+  icon : string;
+  gifs : string[];
+  // Name of binded function to be triggered (?) ou entao cenas com serviçoes, tenho de ver como fazer isto em angular
+  actionFnc : string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onGifClick() {
+    // TODO sth with the action that it will receive
   }
 
 }
