90033b7796a895a0fa15030e233e8ef983fcbc2f
EdgarACarneiro <up201503784@fe.up.pt>
2018-07-09 20:04:12 +0100

    Added routing

--- angularGiphyApp/src/app/app.module.ts
+++ angularGiphyApp/src/app/app.module.ts
@@ -2,11 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FunctionalitiesComponent } from './functionalities/functionalities.component';
 import { SearchComponent } from './search/search.component';
 import { GiphsContainerComponent } from './giphs-container/giphs-container.component';
+
+const appRoutes: Routes = [
+  { 
+    path: 'search?=:id',
+    component: GiphsContainerComponent,
+    data: {title: 'FavGiphy: Search'}
+  },
+  { 
+    path: 'favorites',
+    component: GiphsContainerComponent,
+    data: { title: 'FavGiphy: Favorites' }
+  },
+  {
+    path: 'fresh',
+    component: GiphsContainerComponent,
+    data: {title: 'FavGiphy: Fresh'}
+  },
+  { 
+    path: '',
+    redirectTo: '/fresh',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/fresh'
+  }
+];
+
 
 @NgModule({
   declarations: [
@@ -16,6 +45,10 @@ import { GiphsContainerComponent } from './giphs-container/giphs-container.compo
     GiphsContainerComponent,
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     FormsModule,
     AngularFontAwesomeModule
